9844a5eac8254a6a56ee69f08b27aa1bd5091928
guruvyaasMtech <2023tm93628@wilp.bits-pilani.ac.in>
2025-05-04 16:11:34 +0530

    Update RabbitMQConfig.java

--- src/main/java/com/libManager/inventory_service/config/RabbitMQConfig.java
+++ src/main/java/com/libManager/inventory_service/config/RabbitMQConfig.java
@@ -1,71 +1,52 @@
-package com.libManager.inventory_service.consumer;
+package com.libManager.inventory_service.config;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.BindingBuilder.GenericArgumentsConfigurer;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.Exchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    private final String BOOK_CREATED_ROUTING_KEY = "book.created";
+    private final String BOOK_UPDATED_ROUTING_KEY = "book.updated";
 
 
-import com.libManager.inventory_service.model.Inventory;
-import com.libManager.inventory_service.repository.InventoryRepository;
-
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.amqp.support.converter.SimpleMessageConverter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
-
-@Component
-public class InventoryEventListener {
-    @Autowired
-    private InventoryRepository inventoryRepository;
-    private final MessageConverter messageConverter = new SimpleMessageConverter();
-
-    @RabbitListener(queues = "book.created.queue")
-    @Transactional
-    public void handleBookCreatedEvent(org.springframework.amqp.core.Message message) {
-        try {
-            Long event = (Long) messageConverter.fromMessage(message);
-            Long bookId = event;
-            if (!inventoryRepository.existsById(bookId)) {
-                Inventory inventory = new Inventory();
-                inventory.setBookId(bookId);
-                inventory.setQuantity(100);
-                inventoryRepository.save(inventory);
-                System.out.println("Inventory created for book: " + event);
-            } else {
-                System.out.println("Inventory record already exists for book ID: " + bookId + ".  No action taken.");
-            }
-        } catch (Exception e) {
-            System.err.println("Error processing BookCreatedEvent: " + e.getMessage());
-        }
+    @Bean
+    public Queue bookCreatedQueue() {
+        return new Queue("book.created.queue", true);
     }
-    @RabbitListener(queues = "book.updated.queue")
-    @Transactional
-    public void handleBookUpdatedEvent(org.springframework.amqp.core.Message message) {
-        try {
-            String messageBody = new String(message.getBody(), "UTF-8");
-            String[] parts = messageBody.split("/");
-            if (parts.length>0) {
-                Long bookId = Long.parseLong(parts[0]);
-                String action = parts[1];
-                int change=1;
-                if(action.equals("checkout")) {
-                	change=-1;
-                }
 
-                Inventory inventory = inventoryRepository.findById(bookId).orElse(null);
-                if (inventory != null) {
-                    int newQuantity = inventory.getQuantity() + change;
-                    inventory.setQuantity(newQuantity);
-                    inventoryRepository.save(inventory);
-                    System.out.println("Inventory updated for book ID: " + bookId + " New Quantity: " + newQuantity);
-                } else {
-                    System.out.println("Inventory not found for book ID: " + bookId);
-                }
-            } else {
-                System.err.println("Invalid message format: " + messageBody);
-            }
+    @Bean
+    public Binding bookCreatedBinding(Queue bookCreatedQueue, TopicExchange bookCreatedExchange) {
+        return BindingBuilder
+                .bind(bookCreatedQueue)
+                .to(bookCreatedExchange)
+                .with(BOOK_CREATED_ROUTING_KEY);
+    }
 
-        } catch (Exception e) {
-            System.err.println("Error processing BookUpdatedEvent: " + e.getMessage());
-        }
+    @Bean
+    public TopicExchange bookCreatedExchange() {
+        return new TopicExchange("book.exchange");
+    }
+    
+    
+    // New queue and binding for BookUpdatedEvent
+    @Bean
+    public Queue bookUpdatedQueue() {
+        return new Queue("book.updated.queue", true); // Make the queue durable
+    }
+
+    @Bean
+    public Binding bookUpdatedBinding(Queue bookUpdatedQueue, TopicExchange bookCreatedExchange) {
+        return BindingBuilder
+                .bind(bookUpdatedQueue)
+                .to(bookCreatedExchange)
+                .with(BOOK_UPDATED_ROUTING_KEY);
     }
 }
+
+
+
